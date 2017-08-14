053522d6b719d89c05f14c1909d10fe6aa82daf5
jmarkow <jmarkow@cns.bu.edu>
2017-08-13 22:51:54 -0400

    Cleaned up save_figure

--- @schfigure/save_figure.m
+++ @schfigure/save_figure.m
@@ -10,12 +10,16 @@ for i=1:length(OBJ)
 	renderer=sprintf('-%s',OBJ(i).renderer);
 	res=sprintf('-r%i',OBJ(i).resolution);
 	filename=fullfile(OBJ(i).working_dir,OBJ(i).name);
-
+	pdf_gen=false;
 	try
 		if contains(OBJ(i).formats,'eps') || contains(OBJ(i).formats,'all')
 			print(OBJ(i).fig,'-depsc2',renderer,sprintf('%s.eps',filename));
 			% yeah MATLAB is REALLY EFFING GREAT AT MAKE NICE EPS files
 			epsclean(sprintf('%s.eps',filename),sprintf('%s.eps',filename),false,true);
+			if contains(OBJ(i).formats,'pdf') || contains(OBJ(i).formats,'all')
+				[status,output]=system(sprintf('ps2pdf -dEPSCrop "%s.eps" "%s.pdf"',filename,filename));
+				pdf_gen=true;
+			end
 		end
 
 		if contains(OBJ(i).formats,'tiffn') || contains(OBJ(i).formats,'all')
@@ -45,7 +49,7 @@ for i=1:length(OBJ)
 			print(OBJ(i).fig,'-dtiff',renderer,res,sprintf('%s.tiff',filename));
 		end
 
-		if contains(OBJ(i).formats,'pdf') || contains(OBJ(i).formats,'all')
+		if (contains(OBJ(i).formats,'pdf') || contains(OBJ(i).formats,'all')) & ~pdf_gen
 			print(OBJ(i).fig,'-dpdf',renderer,res,sprintf('%s.pdf',filename));
 		end
 
