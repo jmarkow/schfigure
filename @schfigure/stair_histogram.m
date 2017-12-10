2ab805c641f845b03417cbfc6626e40ec8077980
jmarkow <jmarkow@cns.bu.edu>
2017-12-10 16:56:24 -0500

    Bugfix for stair_histogram

--- @schfigure/stair_histogram.m
+++ @schfigure/stair_histogram.m
@@ -9,18 +9,14 @@ opts=struct(...
 
 nparams=length(varargin);
 
-if mod(nparams,2)>0
-	error('Parameters must be specified as parameter/value pairs!');
-end
-
 opts_names=fieldnames(opts);
 use_params=[];
 
-for i=1:2:nparams
+for i=1:nparams
   if any(strcmp(varargin{i},opts_names))
     opts.(varargin{i})=varargin{i+1};
+    use_params=[use_params i i+1];
   end
-  use_params=[use_params i i+1];
 end
 
 varargin(use_params)=[];
